1c1e783ee5d1d3d6237646369ad2a2b0305e9e9f
den-prahms-ihr-ivo <ivo@ivolino.de>
2023-09-23 17:56:41 +0200

    Added first passing test

---
+++ tests/InitialTests/test_buzz.py
@@ -0,0 +1,12 @@
+import unittest
+from unittest.mock import patch, Mock
+# from model import SimpleAPI
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, True)  # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()
