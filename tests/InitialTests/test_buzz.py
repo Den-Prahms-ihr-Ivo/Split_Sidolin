26b70dd6f39cfe5e7d2c751cb003951d3688fddd
den-prahms-ihr-ivo <ivo@ivolino.de>
2023-09-23 17:57:18 +0200

    Updated Unit Test

--- tests/InitialTests/test_buzz.py
+++ tests/InitialTests/test_buzz.py
@@ -5,7 +5,7 @@ from unittest.mock import patch, Mock
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
-        self.assertEqual(True, True)  # add assertion here
+        self.assertEqual(True, False)  # add assertion here
 
 
 if __name__ == '__main__':
